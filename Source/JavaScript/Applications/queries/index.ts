27b5551fc77b203e02f42b50582a8aafccd8cf98
Einar Ingebrigtsen <einari@me.com>
2024-10-19 07:37:49 +0200

    Exposing QueryProvider

--- Source/JavaScript/Applications/queries/index.ts
+++ Source/JavaScript/Applications/queries/index.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+export * from './IQuery';
 export * from './IQueryFor';
 export * from './Paging';
 export * from './Sorting';
@@ -16,3 +17,5 @@ export * from './ObservableQueryFor';
 export * from './IObservableQueryConnection';
 export * from './ObservableQueryConnection';
 export * from './ObservableQuerySubscription';
+export * from './IQueryProvider';
+export * from './QueryProvider';
