5a24def2e9d547a83b98c7a51a6c3e902db830e3
woksin <sindrewilting@gmail.com>
2024-11-14 14:36:58 +0100

    Fix specs for .net 9

--- Source/DotNET/Applications.Specs/Identity/for_IdentityProviderEndpoint/when_handling/and_identity_is_valid_with_multiple_roles.cs
+++ Source/DotNET/Applications.Specs/Identity/for_IdentityProviderEndpoint/when_handling/and_identity_is_valid_with_multiple_roles.cs
@@ -22,5 +22,5 @@ public class and_identity_is_valid_with_multiple_roles : given.a_valid_identity_
     [Fact] void should_invoke_identity_provider() => identity_provider.Received(1).Provide(Arg.Any<IdentityProviderContext>());
     [Fact] void should_pass_first_role_to_identity_provider() => identity_provider_context.Claims.ShouldContain(_ => _.Key == ClaimTypes.Role && _.Value == "role1");
     [Fact] void should_pass_second_role_to_identity_provider() => identity_provider_context.Claims.ShouldContain(_ => _.Key == ClaimTypes.Role && _.Value == "role2");
-    [Fact] void should_set_status_code_to_200() => response.Received(1).StatusCode = 200;
+    [Fact] void should_set_status_code_to_200() => response.StatusCode.ShouldEqual(200);
 }
