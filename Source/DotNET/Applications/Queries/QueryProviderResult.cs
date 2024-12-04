6958fdea2dc6f87451f1fbb1c71c5219bca07d26
Einar Ingebrigtsen <einari@me.com>
2024-12-04 16:23:41 +0100

    Supporting single objects - we were forcing returns to be IEnumerable

--- Source/DotNET/Applications/Queries/QueryProviderResult.cs
+++ Source/DotNET/Applications/Queries/QueryProviderResult.cs
@@ -1,13 +1,11 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Collections;
-
 namespace Cratis.Applications.Queries;
 
 /// <summary>
 /// Represents the result of a query provider.
 /// </summary>
 /// <param name="TotalItems">Total items.</param>
-/// <param name="Items">The rendered items.</param>
-public record QueryProviderResult(int TotalItems, IEnumerable Items);
+/// <param name="Data">The rendered data.</param>
+public record QueryProviderResult(int TotalItems, object Data);
