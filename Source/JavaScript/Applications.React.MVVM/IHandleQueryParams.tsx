5a720a1e96823d080f317d7038933b9a84de203e
Einar Ingebrigtsen <einari@me.com>
2025-01-28 18:58:32 +0100

    License header

--- Source/JavaScript/Applications.React.MVVM/IHandleQueryParams.tsx
+++ Source/JavaScript/Applications.React.MVVM/IHandleQueryParams.tsx
@@ -1,8 +1,9 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 /**
  * Represents a component that can handle query parameters, typically a viewModel.
  */
-
-
 export interface IHandleQueryParams<T = object> {
 
     /**
