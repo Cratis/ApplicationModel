a96319f31626a547894ac4bed7bc2de1011afe96
Einar Ingebrigtsen <einari@me.com>
2025-03-14 15:47:17 +0100

    Removing Sample, will reproduce in Samples repo

--- Samples/eCommerce/Basic/API/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-global using Cratis.Applications.Commands;
-global using FluentValidation;
-global using Microsoft.AspNetCore.Mvc;
