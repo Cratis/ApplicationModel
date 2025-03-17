128770aee56fda99f16620df48edf744044ac0e6
woksin <sindrewilting@gmail.com>
2025-03-17 13:16:31 +0100

    Specs

--- Source/DotNET/MongoDB.Specs/for_QueryContextAwareSet/when_remove_and_add_last_in_query/and_paging_is_on_and_there_are_fewer_documents_than_page_limit.cs
+++ Source/DotNET/MongoDB.Specs/for_QueryContextAwareSet/when_remove_and_add_last_in_query/and_paging_is_on_and_there_are_fewer_documents_than_page_limit.cs
@@ -1,11 +1,8 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 using MongoDB.Driver;
 using SortDirection = Cratis.Applications.Queries.SortDirection;
 namespace Cratis.Applications.MongoDB.for_QueryContextAwareSet.when_remove_and_add_last_in_query;
 
-public class and_there_are_no_documents_in_the_query : Specification
+public class and_paging_is_on_and_there_are_fewer_documents_than_page_limit : Specification
 {
     QueryContextAwareSet<SomeClassWithSomeId> set;
 
@@ -19,9 +16,9 @@ public class and_there_are_no_documents_in_the_query : Specification
     {
         set = new(QueryContextBuilder.New()
             .WithSorting(new(nameof(SomeClassWithSomeId.Value), SortDirection.Ascending))
-            .WithPageSize(4)
+            .WithPageSize(10)
             .Build());
-        queryDocuments = [];
+        queryDocuments = [new(Guid.NewGuid(), 49), new(Guid.NewGuid(), 46), new(Guid.NewGuid(), 52)];
         firstItem = new(Guid.NewGuid(), 40);
         secondItem = new(Guid.NewGuid(), 41);
         thirdItem = new(Guid.NewGuid(), 42);
@@ -34,5 +31,5 @@ public class and_there_are_no_documents_in_the_query : Specification
 
     async Task Because() => await set.RemoveAndAddLastInQuery(thirdItem.Id, new InMemoryFluentFind<SomeClassWithSomeId>(queryDocuments));
 
-    [Fact] void should_have_items_in_correct_order() => Assert.Equal([firstItem, secondItem, fourthItem], set);
+    [Fact] void should_have_items_in_correct_order_and_not_add_from_the_query() => Assert.Equal([firstItem, secondItem, fourthItem], set);
 }
--- Source/DotNET/MongoDB.Specs/for_QueryContextAwareSet/when_remove_and_add_last_in_query/and_there_is_one_document_in_the_query.cs
+++ Source/DotNET/MongoDB.Specs/for_QueryContextAwareSet/when_remove_and_add_last_in_query/and_there_is_one_document_in_the_query.cs
@@ -1,11 +1,8 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 using MongoDB.Driver;
 using SortDirection = Cratis.Applications.Queries.SortDirection;
 namespace Cratis.Applications.MongoDB.for_QueryContextAwareSet.when_remove_and_add_last_in_query;
 
-public class and_there_are_no_documents_in_the_query : Specification
+public class and_there_is_one_document_in_the_query : Specification
 {
     QueryContextAwareSet<SomeClassWithSomeId> set;
 
@@ -21,7 +18,7 @@ public class and_there_are_no_documents_in_the_query : Specification
             .WithSorting(new(nameof(SomeClassWithSomeId.Value), SortDirection.Ascending))
             .WithPageSize(4)
             .Build());
-        queryDocuments = [];
+        queryDocuments = [new(Guid.NewGuid(), 49)];
         firstItem = new(Guid.NewGuid(), 40);
         secondItem = new(Guid.NewGuid(), 41);
         thirdItem = new(Guid.NewGuid(), 42);
@@ -34,5 +31,5 @@ public class and_there_are_no_documents_in_the_query : Specification
 
     async Task Because() => await set.RemoveAndAddLastInQuery(thirdItem.Id, new InMemoryFluentFind<SomeClassWithSomeId>(queryDocuments));
 
-    [Fact] void should_have_items_in_correct_order() => Assert.Equal([firstItem, secondItem, fourthItem], set);
+    [Fact] void should_have_items_in_correct_order() => Assert.Equal([firstItem, secondItem, fourthItem, queryDocuments.First()], set);
 }
