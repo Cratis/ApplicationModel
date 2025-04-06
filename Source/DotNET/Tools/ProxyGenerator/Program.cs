a53db0ca5a10fea244405433ef54a325ad245adf
Einar Ingebrigtsen <einari@me.com>
2025-04-06 10:16:53 +0200

    Adding support for skipping the deletion of the output folder when generating proxies

--- Source/DotNET/Tools/ProxyGenerator/Program.cs
+++ Source/DotNET/Tools/ProxyGenerator/Program.cs
@@ -8,17 +8,19 @@ Console.WriteLine("Cratis Proxy Generator\n");
 if (args.Length < 2)
 {
     Console.WriteLine("Usage: ");
-    Console.WriteLine("  Cratis.ProxyGenerator <assembly> <output-path> [segments-to-skip]");
+    Console.WriteLine("  Cratis.ProxyGenerator <assembly> <output-path> [segments-to-skip] [--skip-output-deletion]");
     return 1;
 }
 var assemblyFile = Path.GetFullPath(args[0]);
 var outputPath = Path.GetFullPath(args[1]);
 var segmentsToSkip = args.Length > 2 ? int.Parse(args[2]) : 0;
+var skipOutputDeletion = args.Any(_ => _ == "--skip-output-deletion");
 
 var result = await Generator.Generate(
     assemblyFile,
     outputPath,
     segmentsToSkip,
     Console.WriteLine,
-    Console.Error.WriteLine);
+    Console.Error.WriteLine,
+    skipOutputDeletion);
 return result ? 0 : 1;
