83d550fea33542e85314b04b7a697da8b5354144
Einar <einari@me.com>
2025-09-08 18:58:19 +0200

    Fleshing out a design

--- Source/DotNET/Applications/Commands/CommandResult.cs
+++ Source/DotNET/Applications/Commands/CommandResult.cs
@@ -1,8 +1,11 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using Cratis.Applications.Validation;
 using Cratis.Execution;
+using OneOf;
 
 namespace Cratis.Applications.Commands;
 
@@ -52,6 +55,13 @@ public class CommandResult
     /// Gets the stack trace if there was an exception.
     /// </summary>
     public string ExceptionStackTrace { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Creates a new <see cref="CommandResult"/> representing a missing handler.
+    /// </summary>
+    /// <param name="type">The type of command that is missing a handler.</param>
+    /// <returns>A <see cref="CommandResult"/>.</returns>
+    public static CommandResult MissingHandler(Type type) => new() { ExceptionMessages = [$"No handler found for command of type {type}"] };
 }
 
 /// <summary>
@@ -69,4 +79,61 @@ public class CommandResult<T> : CommandResult
     /// Optional response object. Controller actions representing a command can optionally return a response as any type, this is where it would be.
     /// </summary>
     public T? Response { get; init; }
+}
+
+public interface ICommandResponseValueHandler<TValue>
+{
+    Task<CommandResult> Handle(TValue value);
+}
+
+public interface ICommandResponseValueHandlers
+{
+    Task<CommandResult> Handle(object value);
+}
+
+public interface ICommandPipeline
+{
+    Task<CommandResult> Execute(object command);
+}
+
+public interface ICommandHandler
+{
+    Task<OneOf<object, ITuple>> Handle(object command);
+}
+
+public interface ICommandHandlerProvider
+{
+    bool TryGetHandlerFor(object command, [NotNullWhen(true)] out ICommandHandler? handler);
+}
+
+public interface ICommandHandlerProviders
+{
+    bool TryGetHandlerFor(object command, [NotNullWhen(true)] out ICommandHandler? handler);
+}
+
+public class CommandPipeline(
+    ICommandHandlerProviders handlerProviders,
+    ICommandResponseValueHandlers valueHandlers) : ICommandPipeline
+{
+    /// <inheritdoc/>
+    public async Task<CommandResult> Execute(object command)
+    {
+        handlerProviders.TryGetHandlerFor(command, out var commandHandler);
+        if (commandHandler is null)
+        {
+            return CommandResult.MissingHandler(command.GetType());
+        }
+
+        var result = await commandHandler.Handle(command);
+
+        // If the first value is not a tuple
+        //    If the first value is a OneOf
+        //        Loop through each value in the OneOf and handle the value
+        // Else (it is a tuple)
+        //    Use the first value in the tuple as the response for the command
+        //    Loop through the rest of the values in the tuple and handle each value
+
+        // result = await valueHandlers.Handle(result);
+        throw new NotImplementedException();
+    }
 }
