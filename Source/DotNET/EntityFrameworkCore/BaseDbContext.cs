c13adfa2005b12e010af4bd552ee0f95bde69482
Einar <einari@me.com>
2025-09-15 10:57:22 +0200

    Added a BaseDbContext

---
+++ Source/DotNET/EntityFrameworkCore/BaseDbContext.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Applications.EntityFrameworkCore.Json;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cratis.Applications.EntityFrameworkCore;
+
+/// <summary>
+/// Base class for DbContexts that needs standard things configured.
+/// </summary>
+/// <param name="options">The options to be used by a <see cref="DbContext"/>.</param>
+public class BaseDbContext(DbContextOptions options) : DbContext(options)
+{
+    /// <inheritdoc/>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyJsonConversion(Database.ProviderName);
+        base.OnModelCreating(modelBuilder);
+    }
+}
