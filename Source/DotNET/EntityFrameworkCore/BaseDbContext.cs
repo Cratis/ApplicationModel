a7d931172c03e9c0e2fcb0af358801ed001dc2af
Einar <einari@me.com>
2025-09-16 14:07:04 +0200

    Including correct namespace

--- Source/DotNET/EntityFrameworkCore/BaseDbContext.cs
+++ Source/DotNET/EntityFrameworkCore/BaseDbContext.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Cratis.Applications.EntityFrameworkCore.Concepts;
 using Cratis.Applications.EntityFrameworkCore.Json;
 using Microsoft.EntityFrameworkCore;
 
