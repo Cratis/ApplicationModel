1d918d7d6429c54d888c0b35d973f9f4a8c504c9
Einar <einari@me.com>
2025-09-27 09:37:14 +0200

    Expanding for functionality needed for the new query pipeline

--- Source/DotNET/Applications/Queries/QueryResult.cs
+++ Source/DotNET/Applications/Queries/QueryResult.cs
@@ -24,7 +24,7 @@ public class QueryResult
     /// <summary>
     /// Gets the <see cref="CorrelationId"/> associated with the command.
     /// </summary>
-    public CorrelationId CorrelationId { get; init; } = new(Guid.Empty);
+    public CorrelationId CorrelationId { get; set; } = new(Guid.Empty);
 
     /// <summary>
     /// The data returned.
@@ -39,7 +39,7 @@ public class QueryResult
     /// <summary>
     /// Gets whether the query was authorized to execute.
     /// </summary>
-    public bool IsAuthorized { get; init; } = true;
+    public bool IsAuthorized { get; set; } = true;
 
     /// <summary>
     /// Gets whether the query is valid.
@@ -54,7 +54,7 @@ public class QueryResult
     /// <summary>
     /// Gets the validation results for the query.
     /// </summary>
-    public IEnumerable<ValidationResult> ValidationResults { get; init; } = [];
+    public IEnumerable<ValidationResult> ValidationResults { get; set; } = [];
 
     /// <summary>
     /// Gets any exception messages that might have occurred.
@@ -64,5 +64,36 @@ public class QueryResult
     /// <summary>
     /// Gets the stack trace if there was an exception.
     /// </summary>
-    public string ExceptionStackTrace { get; init; } = string.Empty;
+    public string ExceptionStackTrace { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Creates a new <see cref="QueryResult"/> representing a missing performer.
+    /// </summary>
+    /// <param name="name">The name of the query that is missing a performer.</param>
+    /// <returns>A <see cref="QueryResult"/>.</returns>
+    public static QueryResult MissingPerformer(QueryName name) => new() { ExceptionMessages = [$"No performer found for query {name}"] };
+
+    /// <summary>
+    /// Creates a new <see cref="QueryResult"/> representing an error.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <returns>A <see cref="QueryResult"/>.</returns>
+    public static QueryResult Error(string message) => new() { ExceptionMessages = [message] };
+
+    /// <summary>
+    /// Merges the results of one or more <see cref="QueryResult"/> instances into this.
+    /// </summary>
+    /// <param name="queryResults">Params of <see cref="QueryResult"/> to merge with.</param>
+    public void MergeWith(params QueryResult[] queryResults)
+    {
+        IsAuthorized = IsAuthorized && queryResults.All(r => r.IsAuthorized);
+        ValidationResults = [.. ValidationResults, .. queryResults.SelectMany(r => r.ValidationResults)];
+        ExceptionMessages = [.. ExceptionMessages, .. queryResults.SelectMany(r => r.ExceptionMessages)];
+        ExceptionStackTrace = string.Join(Environment.NewLine, new[] { ExceptionStackTrace }.Concat(queryResults.Select(r => r.ExceptionStackTrace)));
+        Data = queryResults.Select(r => r.Data).FirstOrDefault() ?? Data;
+        if (ExceptionStackTrace.StartsWith(Environment.NewLine))
+        {
+            ExceptionStackTrace = ExceptionStackTrace[Environment.NewLine.Length..];
+        }
+    }
 }
