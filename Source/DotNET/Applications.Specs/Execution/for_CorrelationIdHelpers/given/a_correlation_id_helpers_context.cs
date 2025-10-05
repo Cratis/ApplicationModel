b0780b32f9bc1caa9518ae44cc7d426c0b41938f
Einar <einari@me.com>
2025-10-05 09:52:42 +0200

    Added specs for the correlation id helpers

---
+++ Source/DotNET/Applications.Specs/Execution/for_CorrelationIdHelpers/given/a_correlation_id_helpers_context.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Execution;
+using Microsoft.AspNetCore.Http;
+
+namespace Cratis.Applications.Execution.for_CorrelationIdHelpers.given;
+
+public class a_correlation_id_helpers_context : Specification
+{
+    protected HttpContext _httpContext;
+    protected HttpRequest _httpRequest;
+    protected IHeaderDictionary _headers;
+    protected ICorrelationIdAccessor _correlationIdAccessor;
+    protected ICorrelationIdModifier _correlationIdModifier;
+    protected CorrelationIdOptions _correlationIdOptions;
+    protected CorrelationId _currentCorrelationId;
+    protected IDictionary<object, object?> _httpContextItems;
+
+    void Establish()
+    {
+        _correlationIdOptions = new CorrelationIdOptions
+        {
+            HttpHeader = Constants.DefaultCorrelationIdHeader
+        };
+
+        _correlationIdAccessor = Substitute.For<ICorrelationIdAccessor, ICorrelationIdModifier>();
+        _correlationIdModifier = _correlationIdAccessor as ICorrelationIdModifier;
+        _currentCorrelationId = CorrelationId.NotSet;
+        _correlationIdAccessor.Current.Returns(_ => _currentCorrelationId);
+        _correlationIdModifier
+            .When(c => c.Modify(Arg.Any<CorrelationId>()))
+            .Do(c => _currentCorrelationId = c.Arg<CorrelationId>());
+
+        _httpContext = Substitute.For<HttpContext>();
+        _httpRequest = Substitute.For<HttpRequest>();
+        _headers = Substitute.For<IHeaderDictionary>();
+        _httpContextItems = new Dictionary<object, object?>();
+
+        _httpContext.Request.Returns(_httpRequest);
+        _httpRequest.Headers.Returns(_headers);
+        _httpContext.Items.Returns(_httpContextItems);
+    }
+}
---
+++ Source/DotNET/Applications.Specs/Execution/for_CorrelationIdHelpers/when_handling_correlation_id/with_invalid_guid_and_current_not_set.cs
@@ -0,0 +1,26 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Execution;
+using Microsoft.Extensions.Primitives;
+
+namespace Cratis.Applications.Execution.for_CorrelationIdHelpers.when_handling_correlation_id;
+
+public class with_invalid_guid_and_current_not_set : given.a_correlation_id_helpers_context
+{
+    string _correlationIdAsString;
+
+    void Establish()
+    {
+        _correlationIdAsString = "invalid-guid-string";
+        _headers[Constants.DefaultCorrelationIdHeader].Returns(new StringValues(_correlationIdAsString));
+        _currentCorrelationId = CorrelationId.NotSet;
+    }
+
+    void Because() => _httpContext.HandleCorrelationId(_correlationIdAccessor, _correlationIdOptions);
+
+    [Fact] void should_generate_new_correlation_id() => _currentCorrelationId.ShouldNotEqual(CorrelationId.NotSet);
+    [Fact] void should_set_current_correlation_id() => _correlationIdModifier.Received(1).Modify(_currentCorrelationId);
+    [Fact] void should_set_correlation_id_in_http_context_items() => _httpContextItems[Constants.CorrelationIdItemKey].ShouldEqual(_currentCorrelationId);
+    [Fact] void should_set_correlation_id_in_request_header() => _headers.Received()[Constants.DefaultCorrelationIdHeader] = _currentCorrelationId.ToString();
+}
