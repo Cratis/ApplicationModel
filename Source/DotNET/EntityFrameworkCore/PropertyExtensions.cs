615e695fad560e656f02666f70c624fab2fd60ac
Einar <einari@me.com>
2025-10-10 22:13:27 +0200

    Adding a way to get consistency for Guid properties with lower casing them using a value converter

---
+++ Source/DotNET/EntityFrameworkCore/PropertyExtensions.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Cratis.Applications.EntityFrameworkCore;
+
+/// <summary>
+/// Extensions for configuring properties in Entity Framework Core.
+/// </summary>
+public static class PropertyExtensions
+{
+    static readonly ValueConverter<Guid, string> _guidValueConverter = new(
+        guid => guid.ToString("D"),
+        str => Guid.Parse(str));
+
+    /// <summary>
+    /// Configures the property to use a GUID representation that is compatible across different database providers.
+    /// </summary>
+    /// <param name="propertyBuilder">The property builder to configure.</param>
+    /// <returns>The configured property builder.</returns>
+    public static PropertyBuilder AsGuid(this PropertyBuilder propertyBuilder) =>
+        propertyBuilder.HasConversion(_guidValueConverter);
+}
