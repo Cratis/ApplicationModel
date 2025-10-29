2917577d5309ccbd887c797fd05fc2987a9ef828
Einar <einari@me.com>
2025-10-29 11:58:55 +0100

    Fixing specs after change

--- Source/DotNET/Applications.Specs/Identity/for_IdentityProviderEndpoint/when_handling/and_user_is_not_authenticated.cs
+++ Source/DotNET/Applications.Specs/Identity/for_IdentityProviderEndpoint/when_handling/and_user_is_not_authenticated.cs
@@ -1,20 +1,18 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Security.Claims;
-
 namespace Cratis.Applications.Identity.for_IdentityProviderEndpoint.when_handling;
 
 public class and_user_is_not_authenticated : given.an_identity_provider_endpoint
 {
     void Establish()
     {
-        var identity = new ClaimsIdentity(); // Not authenticated
-        _httpContext.User = new ClaimsPrincipal(identity);
+        _identityProviderResultHandler.GenerateFromCurrentContext().Returns(IdentityProviderResult.Anonymous);
     }
 
-    Task Because() => _endpoint.Handler(_request, _response);
+    Task Because() => _endpoint.Handler(_response);
 
-    [Fact] void should_not_invoke_identity_provider() => _identityProvider.DidNotReceive().Provide(Arg.Any<IdentityProviderContext>());
+    [Fact] void should_call_generate_from_current_context() => _identityProviderResultHandler.Received(1).GenerateFromCurrentContext();
+    [Fact] void should_not_call_write() => _identityProviderResultHandler.DidNotReceive().Write(Arg.Any<IdentityProviderResult>());
     [Fact] void should_set_status_code_to_401() => _response.StatusCode.ShouldEqual(401);
 }
