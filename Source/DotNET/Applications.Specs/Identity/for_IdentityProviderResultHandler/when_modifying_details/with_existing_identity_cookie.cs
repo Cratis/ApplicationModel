c7728de90b04f69df70bbbd262b930299a12a939
Einar <einari@me.com>
2025-10-29 12:33:51 +0100

    Simplifying specs

--- Source/DotNET/Applications.Specs/Identity/for_IdentityProviderResultHandler/when_modifying_details/with_existing_identity_cookie.cs
+++ Source/DotNET/Applications.Specs/Identity/for_IdentityProviderResultHandler/when_modifying_details/with_existing_identity_cookie.cs
@@ -14,14 +14,14 @@ public class with_existing_identity_cookie : given.an_identity_provider_result_h
     IdentityProviderResult<TestDetails> _originalResult;
     string _base64Json;
     string _responseContent;
-    IdentityProviderResult _responseResult;
+    IdentityProviderResult<TestDetails> _responseResult;
     Microsoft.Net.Http.Headers.SetCookieHeaderValue _identityCookie;
-    IdentityProviderResult _cookieResult;
+    IdentityProviderResult<TestDetails> _cookieResult;
 
     void Establish()
     {
-        _originalDetails = new TestDetails { Department = "Engineering", Role = "Developer" };
-        _modifiedDetails = new TestDetails { Department = "Marketing", Role = "Manager" };
+        _originalDetails = new TestDetails("Engineering", "Developer");
+        _modifiedDetails = new TestDetails("Marketing", "Manager");
 
         _originalResult = new IdentityProviderResult<TestDetails>(
             new IdentityId("user123"),
@@ -42,11 +42,19 @@ public class with_existing_identity_cookie : given.an_identity_provider_result_h
     async Task Because()
     {
         await _handler.ModifyDetails<TestDetails>(details => _modifiedDetails);
+        await GetResults();
+    }
 
+    [Fact] void should_write_modified_result_to_response() => _responseResult.Details.ShouldEqual(_modifiedDetails);
+
+    [Fact] void should_set_new_identity_cookie_with_modified_details() => _cookieResult.Details.ShouldEqual(_modifiedDetails);
+
+    async Task GetResults()
+    {
         _httpContext.Response.Body.Position = 0;
         var reader = new StreamReader(_httpContext.Response.Body);
         _responseContent = await reader.ReadToEndAsync();
-        _responseResult = JsonSerializer.Deserialize<IdentityProviderResult>(_responseContent, _serializerOptions)!;
+        _responseResult = JsonSerializer.Deserialize<IdentityProviderResult<TestDetails>>(_responseContent, _serializerOptions)!;
 
         var cookies = _httpContext.Response.GetTypedHeaders().SetCookie;
         _identityCookie = cookies.FirstOrDefault(c => c.Name == IdentityProviderResultHandler.IdentityCookieName)!;
@@ -54,27 +62,8 @@ public class with_existing_identity_cookie : given.an_identity_provider_result_h
         var cookieValue = _identityCookie.Value.ToString();
         var urlDecodedValue = Uri.UnescapeDataString(cookieValue);
         var decodedJson = Encoding.UTF8.GetString(Convert.FromBase64String(urlDecodedValue));
-        _cookieResult = JsonSerializer.Deserialize<IdentityProviderResult>(decodedJson, _serializerOptions)!;
+        _cookieResult = JsonSerializer.Deserialize<IdentityProviderResult<TestDetails>>(decodedJson, _serializerOptions)!;
     }
 
-    [Fact] void should_write_modified_result_to_response()
-    {
-        var detailsJson = JsonSerializer.Serialize(_responseResult.Details, _serializerOptions);
-        var expectedDetailsJson = JsonSerializer.Serialize(_modifiedDetails, _serializerOptions);
-        detailsJson.ShouldEqual(expectedDetailsJson);
-    }
-
-    [Fact] void should_set_new_identity_cookie_with_modified_details()
-    {
-        _identityCookie.ShouldNotBeNull();
-        var detailsJson = JsonSerializer.Serialize(_cookieResult.Details, _serializerOptions);
-        var expectedDetailsJson = JsonSerializer.Serialize(_modifiedDetails, _serializerOptions);
-        detailsJson.ShouldEqual(expectedDetailsJson);
-    }
-
-    public class TestDetails
-    {
-        public string Department { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
-    }
+    public record TestDetails(string Department, string Role);
 }
