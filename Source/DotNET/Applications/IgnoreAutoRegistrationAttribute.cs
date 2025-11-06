abf4251319e72243cbd34cc3efc54f14ffa6feae
Einar <einari@me.com>
2025-11-06 10:24:33 +0100

    Adding a way to explicitly ignore auto registration of DbContexts

---
+++ Source/DotNET/Applications/IgnoreAutoRegistrationAttribute.cs
@@ -0,0 +1,14 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Applications;
+
+/// <summary>
+/// Represents an attribute that indicates that a class should be ignored during automatic registration.
+/// </summary>
+/// <remarks>
+/// This attribute is supported by different systems in the Application Model to indicate that a class should be ignored
+/// by automatic registration mechanisms.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class IgnoreAutoRegistrationAttribute : Attribute;
