0b1a643236489a64a24dd3b193623c26643325c1
Einar <einari@me.com>
2025-11-22 16:13:04 +0100

    Handling "Collection not found" which can happen on MongoDB RU in Azure CosmosDB
    The behavior is different than from a normal MongoDB - if a collection does not
    exist on a regular MongoDB instance, you will just be given an empty cursor of not
    an instance. While in Azure it will come back with an error "Collection not found".

--- Source/DotNET/MongoDB/Resilience/MongoCollectionInterceptor.cs
+++ Source/DotNET/MongoDB/Resilience/MongoCollectionInterceptor.cs
@@ -11,7 +11,7 @@ namespace Cratis.Applications.MongoDB.Resilience;
 /// Represents an interceptor for <see cref="IMongoCollection{TDocument}"/>.
 /// </summary>
 /// <remarks>
-/// Initializes a new instance of the <see cref="MongoCollectionInterceptorForReturnValues"/> class.
+/// Initializes a new instance of the <see cref="MongoCollectionInterceptor"/> class.
 /// </remarks>
 /// <param name="resiliencePipeline">The <see cref="ResiliencePipeline"/> to use.</param>
 /// <param name="openConnectionSemaphore">The <see cref="SemaphoreSlim"/> for keeping track of open connections.</param>
@@ -19,38 +19,36 @@ public class MongoCollectionInterceptor(
     ResiliencePipeline resiliencePipeline,
     SemaphoreSlim openConnectionSemaphore) : IInterceptor
 {
+    const string CollectionNotFoundMessage = "Collection not found";
+
     /// <inheritdoc/>
     public void Intercept(IInvocation invocation)
     {
         var tcs = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
         invocation.ReturnValue = tcs.Task;
 
-        var cancellationToken = invocation.Arguments
-            .FirstOrDefault(argument => argument is CancellationToken) as CancellationToken? ?? CancellationToken.None;
+        var cancellationToken = ExtractCancellationToken(invocation);
 
 #pragma warning disable CA2012 // Use ValueTasks correctly
         resiliencePipeline.ExecuteAsync(
             async _ =>
             {
-                if (!await openConnectionSemaphore.WaitAsync(1000, cancellationToken))
+                if (!await TryAcquireSemaphore(tcs, cancellationToken))
                 {
-                    tcs.SetException(new TimeoutException("Failed to acquire semaphore."));
                     return ValueTask.CompletedTask;
                 }
 
                 try
                 {
-                    var result = (invocation.Method.Invoke(invocation.InvocationTarget, invocation.Arguments) as Task)!;
-                    await result.ConfigureAwait(false);
-
-                    if (result.IsCanceled)
-                        tcs.SetCanceled();
-                    else
-                        tcs.SetResult();
+                    await ExecuteMongoOperation(invocation, tcs);
                 }
                 catch (OperationCanceledException)
                 {
                     tcs.SetCanceled();
+                }
+                catch (MongoCommandException ex) when (ex.Message.Contains(CollectionNotFoundMessage, StringComparison.OrdinalIgnoreCase))
+                {
+                    tcs.SetResult();
                 }
                 catch (Exception ex)
                 {
@@ -65,5 +63,33 @@ public class MongoCollectionInterceptor(
             },
             cancellationToken);
 #pragma warning restore CA2012 // Use ValueTasks correctly
+    }
+
+    static CancellationToken ExtractCancellationToken(IInvocation invocation) =>
+        invocation.Arguments.FirstOrDefault(argument => argument is CancellationToken) as CancellationToken? ?? CancellationToken.None;
+
+    static async Task ExecuteMongoOperation(IInvocation invocation, TaskCompletionSource tcs)
+    {
+        var result = (invocation.Method.Invoke(invocation.InvocationTarget, invocation.Arguments) as Task)!;
+        await result.ConfigureAwait(false);
+
+        if (result.IsCanceled)
+        {
+            tcs.SetCanceled();
+        }
+        else
+        {
+            tcs.SetResult();
+        }
+    }
+
+    async Task<bool> TryAcquireSemaphore(TaskCompletionSource tcs, CancellationToken cancellationToken)
+    {
+        if (!await openConnectionSemaphore.WaitAsync(1000, cancellationToken))
+        {
+            tcs.SetException(new TimeoutException("Failed to acquire semaphore."));
+            return false;
+        }
+        return true;
     }
 }
