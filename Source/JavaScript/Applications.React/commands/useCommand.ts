9c0bd67f89f278c07086e3995f4bccec3e18ef16
Einar <einari@me.com>
2025-11-25 10:01:53 +0100

    Forward the generic arguments to get the correct command definition

--- Source/JavaScript/Applications.React/commands/useCommand.ts
+++ Source/JavaScript/Applications.React/commands/useCommand.ts
@@ -17,7 +17,7 @@ export type ClearCommandValues = () => void;
  * @param initialValues Any initial values to set for the command.
  * @returns Tuple with the command, a {@link SetCommandValues<TCommandContent>} delegate to set values on command and {@link ClearCommandValues} delegate clear values.
  */
-export function useCommand<TCommand extends Command, TCommandContent>(commandType: Constructor<TCommand>, initialValues?: TCommandContent): [TCommand, SetCommandValues<TCommandContent>, ClearCommandValues] {
+export function useCommand<TCommand extends Command<TCommandContent>, TCommandContent = object>(commandType: Constructor<TCommand>, initialValues?: TCommandContent): [TCommand, SetCommandValues<TCommandContent>, ClearCommandValues] {
     const command = useRef<TCommand | null>(null);
     const [hasChanges, setHasChanges] = useState(false);
     const applicationModel = useContext(ApplicationModelContext);
