8a91052f0c33e0392b662012a87ec7a1c120d81a
Einar <einari@me.com>
2025-11-26 07:48:33 +0100

    Introducing ArcBuilder - not implemented, just the contract

---
+++ Source/DotNET/Arc/IArcBuilder.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cratis.Arc;
+
+/// <summary>
+/// Defines a builder for configuring Arc services or extensions.
+/// </summary>
+public interface IArcBuilder
+{
+    /// <summary>
+    /// Gets the service collection to which services can be added.
+    /// </summary>
+    IServiceCollection Services { get; }
+}
--- Source/DotNET/Chronicle/ChronicleBuilderExtensions.cs
+++ Source/DotNET/Chronicle/ChronicleBuilderExtensions.cs
@@ -1,25 +1,22 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Cratis.Chronicle.AspNetCore;
+namespace Cratis.Arc;
 
 /// <summary>
-/// Extension methods for <see cref="IChronicleBuilder"/> for adding ApplicationModel support.
+/// Extension methods for <see cref="IArcBuilder"/> for adding Chronicle support.
 /// </summary>
 public static class ChronicleBuilderExtensions
 {
     /// <summary>
     /// Adds ApplicationModel support to Chronicle configuration.
     /// </summary>
-    /// <param name="builder">The <see cref="IChronicleBuilder"/> to add to.</param>
-    /// <returns><see cref="IChronicleBuilder"/> for continuation.</returns>
-    public static IChronicleBuilder WithApplicationModel(this IChronicleBuilder builder)
+    /// <param name="builder">The <see cref="IArcBuilder"/> to add to.</param>
+    /// <returns><see cref="IArcBuilder"/> for continuation.</returns>
+    public static IArcBuilder WithChronicle(this IArcBuilder builder)
     {
-        builder.Services.AddAggregateRoots(builder.ClientArtifactsProvider);
-        builder.Services.AddReadModels(builder.ClientArtifactsProvider);
-
+        // builder.Services.AddAggregateRoots(builder.ClientArtifactsProvider);
+        // builder.Services.AddReadModels(builder.ClientArtifactsProvider);
         return builder;
     }
 }
